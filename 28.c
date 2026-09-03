1fdfe45ea624d001fe14a67662325754af2594f2
elif7707 <113452475+elif7707@users.noreply.github.com>
2026-09-03 18:01:38 +0300

    Create 28.c

---
+++ 28.c
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+
+        int h = haystack.length();
+        int n = needle.length();
+
+            if(n > h){
+                return -1;
+            }
+
+             for(int i = 0; i <= h - n; i++){
+
+                   int j = 0;
+                   while(j < n && haystack[i + j] == needle[j]){
+                        j++;
+                   }
+                    
+                    if(j == n){
+                        return i;
+                    }
+                
+             }
+        
+        return -1;
+    }
+};
